657f64c63d0e998892faf9659f018cf087a8bd79
Graham Sprague <graham.sprague@gmail.com>
2023-01-25 21:25:11 +0000

    Cleared out main so its always empty.

--- main.py
+++
@@ -1,8 +0,0 @@
-username = input('Username:')
-
-password = input('Password:')
-
-pass_len = len(password)
-pass_shade = '*' * pass_len
-
-print(f'{username}, your password {pass_shade} is {pass_len} characters long.')
