aed506042a2ffbfb2030c4b6da8a306d18b189c7
Graham Sprague <graham.sprague@gmail.com>
2023-01-26 19:39:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#fix this code so that it prints a sorted list of all of our friends (alphabetical). Scroll to see answer
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+friends.extend(new_friend)
+friends.sort()
+print(friends)
