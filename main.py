7ce6d1ca389fac832f53ac003566fd31c2fb697c
Graham Sprague <graham.sprague@gmail.com>
2023-01-27 15:27:31 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-#Exercise!
-#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
-picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
-]
+# Find Duplicates
 
-row_output = ''
+some_list = ['a','b','c','b','d','m','n','n']
+some_list.sort()
+compare_str = ''
 
-for row in picture:
-  for column in row:
-    if column == 0:
-      row_output = row_output + ' '
-    else:
-      row_output = row_output + '*'
-  print(row_output)
-  row_output = ''
-  
+for item in some_list:
+  if item != compare_str:
+    compare_str = item
+  else:
+    print(item)
+
