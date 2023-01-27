16f1af81bc391864a35abc3db252208ec508b684
Graham Sprague <graham.sprague@gmail.com>
2023-01-27 15:02:40 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-#enumerator
+#Exercise!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
 
+row_output = ''
 
-for i,char in enumerate(list(range(100))):
-  if char == 50:
-    print(str(char) + '\'s index is ' + str(i))
+for row in picture:
+  for column in row:
+    if column == 0:
+      row_output = row_output + ' '
+    else:
+      row_output = row_output + '*'
+  print(row_output)
+  row_output = ''
+  
