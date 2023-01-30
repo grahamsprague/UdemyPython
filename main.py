2773604ac3a4b4f4d9fe12297811008799cc572d
Graham Sprague <graham.sprague@gmail.com>
2023-01-30 00:18:56 +0000

    cleaned up format

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 
 def highest_even(mylist):
-
   mylist.sort()
   mylist.reverse()
-
   for item in mylist:
-
     if item % 2 == 0:
       return item
       break
-
+      
 print(highest_even([10,1,2,3,4,11]))
