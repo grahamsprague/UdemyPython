bb2a379568f99abc54324624cafb28c5b0fe04f1
Graham Sprague <graham.sprague@gmail.com>
2023-02-01 17:26:07 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,41 +1,19 @@
-from functools import reduce
-
-#zip
-#filter
-#map
-#reduce
 
 
-#1 Capitalize all of the pet names and print the list
-my_pets = ['sisi', 'bibi', 'titi', 'carla']
+#Square
 
-def my_cap(item):
-  return item.capitalize()
+mylist = [5,4,3]
 
-print(list(map(my_cap, my_pets)))
-
-#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
-my_strings = ['a', 'b', 'c', 'd', 'e']
-my_numbers = [5,4,3,2,1]
-
-print(list(zip(my_strings, my_numbers)))
+print(list(map((lambda item: item**2), mylist)))
 
 
-#3 Filter the scores that pass over 50%
-scores = [73, 20, 65, 19, 76, 100, 88]
+#list sorting by second item
 
-def above_50_only(item):
-  return item > 50
+a = [(0,2),(4,3), (10,-1), (9,9)]
 
-print(list(filter(above_50_only, scores)))
+print(a)
 
-#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+#change the default key to the second index[1]
+a.sort(key=lambda x: x[1])
 
-my_numbers.extend(scores)
-
-print(my_numbers)
-
-def accumulator(acc, item):
-  return acc + item
-
-print(reduce(accumulator, my_numbers, 0))
+print(a)
