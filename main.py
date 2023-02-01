63fa07bf1ee2aa8b147fe765469157470129fd79
Graham Sprague <graham.sprague@gmail.com>
2023-02-01 17:49:34 +0000

    added sort

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 #generate a list of dupes, I used a set for the comp becasue it automatically dis-allows dupes. then I wrapped it in a set so it is fully compatible with the org example.
 duplicates = list({char for char in some_list if some_list.count(char) > 1})
 
+duplicates.sort()
 
 print(duplicates)
