f3df6b23197b3c5a643be2d12c6e0bf042b3ee83
Graham Sprague <graham.sprague@gmail.com>
2023-02-02 17:22:28 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
-# Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
-user1 = {
-    'name': 'Sorna',
-    'valid': False #changing this will either run or not run the message_friends function.
-}
+def special_for(iterable):
+  iterator = iter(iterable)
+  while True:
+    try:
+      iterator*5
+      next(iterator)
+    except StopIteration:
+      break
 
-def authenticated(fn):
-  def wrapper(*args, **kwards):
-      if args[0]['valid'] :
-        return fn(*args, **kwards)
-      else:
-        print('User not valid')
-  return wrapper
 
-  
-@authenticated
-def message_friends(user):
-    print('message has been sent')
+class MyGen:
+  counter = 0
+  current = 0
+  previous = 0
 
-message_friends(user1)
+  def __init__(self, first):
+    self.first = first
+
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+
+    # make sure we are under the given value threshold
+    if MyGen.counter < self.first:
+
+      # need to kick start by setting to 1 on second pass.
+      if MyGen.counter > 0 and MyGen.current == 0:
+        MyGen.current += 1
+
+      # increment and calculate
+      MyGen.counter += 1
+      result = MyGen.previous + MyGen.current
+      MyGen.previous = MyGen.current
+      MyGen.current = result
+
+      return result
+    raise StopIteration
+
+gen = MyGen(20)
+for i in gen:
+    print(i)
