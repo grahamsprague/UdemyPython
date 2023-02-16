4b28c496a71c7af3c331eaca5c059c738e82ea01
Graham Sprague <graham.sprague@gmail.com>
2023-02-16 15:09:52 -0500

    moved length check to regex after seeing solution.

--- 193-Regex/password_validator.py
+++ 193-Regex/password_validator.py
@@ -2,11 +2,16 @@
 
 import re
 
-pattern = re.compile(r"([a-zA-Z0-9]+\@*\#*\%*\$*$)")
+pattern = re.compile(r"([a-zA-Z0-9@#%$]{8,}\d)")
 
 string = 'mYtes12345t@#%' # match
 # string = 'mYtes12345t@#%!!!!(*&^&(*&))' # no match
 
 a = pattern.search(string)
+
+if  a != None:
+    print(string + ' is a good password.')
+else:
+    print(string + 'does not meet the requirements. (Must be at least 8 char in length.)')
 
 print(a)
